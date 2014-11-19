050dedebe06517103e9f68f8f4761938e6d5cb10
SteppXXL <Stephan.Praetsch@mercateo.com>
2014-11-19 20:36:18 +0100

    renamed Start to StartEmbeddedJetty

--- src/main/java/com/mercateo/WicketApplication.java
+++ src/main/java/com/mercateo/WicketApplication.java
@@ -7,7 +7,7 @@ import org.apache.wicket.protocol.http.WebApplication;
  * Application object for your web application. If you want to run this
  * application without deploying, run the Start class.
  * 
- * @see com.mercateo.Start#main(String[])
+ * @see com.mercateo.StartEmbeddedJetty#main(String[])
  */
 public class WicketApplication extends WebApplication {
     /**
