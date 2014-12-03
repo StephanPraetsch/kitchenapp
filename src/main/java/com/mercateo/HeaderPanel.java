0851a2aa68c44335eeab892eeefd92432906071a
SteppXXL <Stephan.Praetsch@mercateo.com>
2014-12-03 08:57:20 +0100

    logout

--- src/main/java/com/mercateo/HeaderPanel.java
+++ src/main/java/com/mercateo/HeaderPanel.java
@@ -1,11 +1,29 @@
 package com.mercateo;
 
+import org.apache.wicket.authroles.authentication.AuthenticatedWebSession;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.panel.Panel;
 
 public class HeaderPanel extends Panel {
 
     public HeaderPanel(String id) {
         super(id);
+
+        if (AuthenticatedWebSession.get().isSignedIn()) {
+            add(new Link("logOut") {
+
+                @Override
+                public void onClick() {
+                    AuthenticatedWebSession.get().invalidate();
+                    setResponsePage(getApplication().getHomePage());
+                }
+            });
+
+        } else {
+            add(new Label("logOut"));
+        }
+
     }
 
 }
