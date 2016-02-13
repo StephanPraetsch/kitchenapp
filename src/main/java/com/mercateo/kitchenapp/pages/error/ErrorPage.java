580c02fff012752a673f68b0c6c060d96dba0dce
Stephan Praetsch <Stephan.Praetsch@mercateo.com>
2016-02-13 15:34:37 +0100

    error page

--- src/main/java/com/mercateo/kitchenapp/pages/error/ErrorPage.java
+++ src/main/java/com/mercateo/kitchenapp/pages/error/ErrorPage.java
@@ -1,13 +1,17 @@
 package com.mercateo.kitchenapp.pages.error;
 
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 import com.mercateo.kitchenapp.pages.general.GeneralPage;
+import com.mercateo.kitchenapp.util.WicketConstants;
 
 public class ErrorPage extends GeneralPage {
 
     public ErrorPage(PageParameters params) {
         super(params);
+        replace(new Label(MESSAGE_ID, "Ein Fehler ist aufgetreten: " + params.get(
+                WicketConstants.STATUS)));
     }
 
 }
