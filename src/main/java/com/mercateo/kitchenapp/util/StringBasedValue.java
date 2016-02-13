9cc3857ce97603eb86aac67dc659cc44bf480553
Stephan Praetsch <Stephan.Praetsch@mercateo.com>
2016-02-13 15:01:57 +0100

    null check

--- src/main/java/com/mercateo/kitchenapp/util/StringBasedValue.java
+++ src/main/java/com/mercateo/kitchenapp/util/StringBasedValue.java
@@ -1,11 +1,13 @@
 package com.mercateo.kitchenapp.util;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 public abstract class StringBasedValue {
 
     private final String value;
 
     public StringBasedValue(String value) {
-        this.value = value;
+        this.value = checkNotNull(value);
     }
 
     public String asString() {
