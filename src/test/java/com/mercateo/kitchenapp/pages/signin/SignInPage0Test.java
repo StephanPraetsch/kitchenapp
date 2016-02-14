83c72736204678a1a396ecc831f3e91ec17354be
Stephan Praetsch <Stephan.Praetsch@mercateo.com>
2016-02-14 22:31:20 +0100

    fixed SignInPage0Test

--- src/test/java/com/mercateo/kitchenapp/pages/signin/SignInPage0Test.java
+++ src/test/java/com/mercateo/kitchenapp/pages/signin/SignInPage0Test.java
@@ -7,14 +7,26 @@ import org.junit.Test;
 
 import com.mercateo.kitchenapp.pages.WicketTest;
 
-public class SignInPage3Test extends WicketTest {
+public class SignInPage0Test extends WicketTest {
 
     @Test
-    public void test_render_page() {
+    public void test_render_page() throws Exception {
 
+        // When
         tester.startPage(SignInPage.class);
 
+        // Then
         tester.assertRenderedPage(SignInPage.class);
+
+    }
+
+    @Test
+    public void test_components() {
+
+        // When
+        tester.startPage(SignInPage.class);
+
+        // Then
         tester.assertComponent("signInForm", Form.class);
         tester.assertComponent("signInForm:email", TextField.class);
         tester.assertComponent("signInForm:password", PasswordTextField.class);
