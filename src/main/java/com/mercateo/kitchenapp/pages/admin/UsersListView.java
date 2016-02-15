01986ed32468f76d7f65295b8514558c183c128e
Stephan Praetsch <Stephan.Praetsch@mercateo.com>
2016-02-15 20:28:47 +0100

    use a DataView for more users

--- src/main/java/com/mercateo/kitchenapp/pages/admin/UsersListView.java
+++ src/main/java/com/mercateo/kitchenapp/pages/admin/UsersListView.java
@@ -2,11 +2,11 @@ package com.mercateo.kitchenapp.pages.admin;
 
 import java.util.List;
 
-import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.list.AbstractItem;
+import org.apache.wicket.markup.repeater.Item;
 import org.apache.wicket.markup.repeater.RepeatingView;
-import org.apache.wicket.model.AbstractReadOnlyModel;
+import org.apache.wicket.markup.repeater.data.DataView;
+import org.apache.wicket.markup.repeater.data.ListDataProvider;
 
 import com.mercateo.kitchenapp.data.User;
 import com.mercateo.kitchenapp.db.UserAccess;
@@ -21,35 +21,22 @@ class UsersListView extends RepeatingView {
 
     private RepeatingView createView(UserAccess userAccess) {
 
-        RepeatingView repeating = new RepeatingView("repeating");
-
         List<User> allUsers = userAccess.listAllUsers();
 
-        int index = 0;
-        for (User user : allUsers) {
+        DataView<User> dataView = new DataView<User>("repeating", new ListDataProvider<>(
+                allUsers)) {
+            @Override
+            public void populateItem(Item<User> item) {
+                User user = item.getModelObject();
+                item.add(new Label(WicketConstants.EMAIL, user.getEmail().asString()));
+                item.add(new Label(WicketConstants.PASSWORD, user.getPassword().asString()));
+                item.add(new Label(WicketConstants.ROLES, user.getUserRoles().toString()));
+            }
+        };
 
-            AbstractItem item = new AbstractItem(repeating.newChildId());
+        dataView.setItemsPerPage(10);
 
-            repeating.add(item);
-
-            item.add(new Label(WicketConstants.EMAIL, user.getEmail().asString()));
-            item.add(new Label(WicketConstants.PASSWORD, user.getPassword().asString()));
-            item.add(new Label(WicketConstants.ROLES, user.getUserRoles().toString()));
-
-            final int idx = index;
-            item.add(AttributeModifier.replace("class", new AbstractReadOnlyModel<String>() {
-                private static final long serialVersionUID = 1L;
-
-                @Override
-                public String getObject() {
-                    return (idx % 2 == 1) ? "even" : "odd";
-                }
-            }));
-
-            index++;
-        }
-
-        return repeating;
+        return dataView;
 
     }
 
